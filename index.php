2f083a686361deaa3253b5958b95a3ef185d0aa4
Garrett Sward <swardg@wfu.edu>
2024-09-03 16:31:48 -0400

    fixed missing closing parentheses on line 6 in index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 echo "readme:<br />";
-$readme_filepath = '../README.md';
+$readme_filepath = './README.md';
 $readme_contents = 'no readme found<br />.';
-if (file_exists('../README.md') {
+if (file_exists($readme_filepath)) {
 	$readme_contents = file_get_contents($readme_filepath);
 }
 echo $readme_contents;
